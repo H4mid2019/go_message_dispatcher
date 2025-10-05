d9d5d635d491bf2dd3103d7d89976be62a0d1518
Hamid <hamidreza.darabi@hotmail.com>
2025-10-06 01:55:46 +0200

    refactor message insertion dynamic msg count, with trackable extension number

--- cmd/add-test-messages/main.go
+++ cmd/add-test-messages/main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
-	"context"
 	"database/sql"
+	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 
 	_ "github.com/lib/pq"
 
 	"github.com/go-message-dispatcher/internal/config"
 )
 
+func randomPhone() string {
+	// Common country codes: 1 (US/CA), 44 (UK), 49 (DE), 90 (TR), 359 (BG), 86 (CN)
+	countryCodes := []string{"1", "44", "49", "90", "359", "86", "91", "33", "39", "81"}
+	countryCode := countryCodes[rand.Intn(len(countryCodes))] // #nosec G404 -- Using math/rand is acceptable for test data generation
+
+	// Generates 9-10 digit, cause the total length is 10-20 chars
+	subscriberDigits := 10
+	if len(countryCode) >= 3 {
+		subscriberDigits = 9 // Shorter number for longer country codes
+	}
+
+	var subscriberNumber int64
+	if subscriberDigits == 10 {
+		subscriberNumber = rand.Int63n(9000000000) + 1000000000 // #nosec G404 -- Using math/rand is acceptable for test data generation
+	} else {
+		subscriberNumber = rand.Int63n(900000000) + 100000000 // #nosec G404 -- Using math/rand is acceptable for test data generation
+	}
+
+	return fmt.Sprintf("+%s%d", countryCode, subscriberNumber)
+}
+
 func main() {
+	msgCounts := flag.Int("msg_count", 3, "How many entries it should put into db.")
+	startNumber := flag.Int("start_number", 0, "default start number to start, simple way to track the messages")
+	flag.Parse()
+
+	if *msgCounts == 0 {
+		log.Println("You can define the count of messages by using msg_count flag")
+		log.Println("By default creates only 3 new messages.")
+	} else {
+		log.Printf("Will create %d messages.", *msgCounts)
+	}
+
+	if *startNumber == 0 {
+		log.Println("You can set the start number at the end of message content, for tracking. using start_number.")
+	} else {
+		log.Printf("Extension number for contents will start from %d", *startNumber)
+	}
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
@@ -23,26 +62,18 @@ func main() {
 	}
 	defer func() { _ = db.Close() }()
 
-	messages := []struct {
-		phone   string
-		content string
-	}{
-		{"+905551111111", "Test message 1"},
-		{"+359888886645", "Test message 2"},
-		{"+905552222222", "Test message 3"},
-		{"+905553333333", "Test message 4"},
-	}
-
-	for _, msg := range messages {
-		_, err := db.ExecContext(context.Background(),
-			"INSERT INTO messages (phone_number, content) VALUES ($1, $2)",
-			msg.phone, msg.content)
+	for counter := range *msgCounts {
+		msgContent := fmt.Sprintf("Test message %d", *startNumber+counter+1)
+		phoneNumber := randomPhone()
+		_, err := db.Exec(`
+			INSERT INTO messages (phone_number, content, scheduled_at, created_at)
+			VALUES ($1, $2, NOW() + INTERVAL '1 minute', NOW())
+		`, phoneNumber, msgContent)
 		if err != nil {
-			log.Printf("Failed to insert message: %v", err)
-		} else {
-			fmt.Printf("Inserted message: %s - %s\n", msg.phone, msg.content)
+			_ = db.Close()                                  // Close connection before exiting
+			log.Fatalf("Failed to insert message: %v", err) //nolint:gocritic // exitAfterDefer is acceptable here as we manually close db
 		}
 	}
 
-	fmt.Println("Test messages added")
+	fmt.Printf("%d test messages added successfully\n", *msgCounts)
 }
